e08ee95c54a521286d756652a80ff1be8ef3c120
AeonJh Fu <aeonjh@nanoleaf.me>
2025-02-13 19:11:15 +0800

    new: implement process.

--- kernel.h
+++ kernel.h
@@ -2,6 +2,20 @@
 
 #include "common.h"
 
+
+#define PROCS_MAX 8      // Maximum number of processes
+
+#define PROC_UNUSED  0   // Unused process control structure
+#define PROC_RUNNING 1   // Runnable process
+
+
+struct process {
+    int pid;             // Process ID
+    int state;           // Process state: PROC_UNUSED or PROC_RUNNING
+    vaddr_t sp;          // Stack pointer
+    uint8_t stack[8192]; // Kernel stack
+};
+
 struct sbiret {
     long error;
     long value;
