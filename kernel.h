fca997c6eec04b97a0d52f94c9bd49f84150d7a5
AeonJh <aeonjh@qq.com>
2026-02-26 10:54:45 +0800

    new: implemnt file system.

--- kernel.h
+++ kernel.h
@@ -74,6 +74,38 @@ struct virtio_blk_req {
     uint8_t status;
 } __attribute__((packed));
 
+#define FILES_MAX     2
+#define DISK_MAX_SIZE align_up(sizeof(struct file) * FILES_MAX, SECTOR_SIZE)
+
+struct tar_header {
+    char name[100];
+    char mode[8];
+    char uid[8];
+    char gid[8];
+    char size[12];
+    char mtime[12];
+    char checksum[8];
+    char type;
+    char linkname[100];
+    char magic[6];
+    char version[2];
+    char uname[32];
+    char gname[32];
+    char devmajor[8];
+    char devminor[8];
+    char prefix[155];
+    char padding[12];
+    char data[];        // Array pointing to the data area following the header
+                        // (flexible array member)
+} __attribute__((packed));
+
+struct file {
+    bool in_use;        // Indicates if this file entry is in use
+    char name[100];     // File name
+    char data[1024];    // File content
+    size_t size;        // File size
+};
+
 #define SATP_SV32    (1u << 31)
 #define PAGE_V       (1 << 0)    // "Valid" bit (entry is enabled)
 #define PAGE_R       (1 << 1)    // Readable
@@ -81,6 +113,7 @@ struct virtio_blk_req {
 #define PAGE_X       (1 << 3)    // Executable
 #define PAGE_U       (1 << 4)    // User (accessible in user mode)
 #define SSTATUS_SPIE (1 << 5)
+#define SSTATUS_SUM  (1 << 18)   // Permit Supervisor User Memory access
 
 #define SCAUSE_ECALL 8
 
--- user.h
+++ user.h
@@ -4,3 +4,5 @@
 __attribute__((noreturn)) void exit(void);
 int getchar(void);
 void putchar(char ch);
+int readfile(const char *filename, char *buf, int len);
+int writefile(const char *filename, const char *buf, int len);
